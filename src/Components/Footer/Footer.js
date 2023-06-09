7cdedd015c0799cd60ef5852eb5b0d40f3d2dac8
Spaces-dev <simplicityspaces@gmail.com>
2023-06-09 05:13:22 -0700

    update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,9 @@ const Footer = () => {
         <footer>
             <div className={classes.topRow}>
                 <div className={classes.ImgBox}>
-                    <img src='https://res.cloudinary.com/da3avivzn/image/upload/v1684308125/SpacesBrandPage/Space.._2_v5jjvp.png' alt='' />
+                    <img src='
+                            https://res.cloudinary.com/da3avivzn/image/upload/v1686300854/SpacesBrandPage/Space.._2_cvsi6j.png'          
+                                />
                 </div>
                 <div className={classes.textBox}>
                     <div className={classes.textBox1} >
@@ -24,15 +26,17 @@ const Footer = () => {
             <div className={classes.bottomRow}>
                 <div className={classes.socialIcons}>
                     <a href='https://twitter.com/Spaces_ui' target="_blank" rel="noopener noreferrer" >
-                        <img src='https://res.cloudinary.com/da3avivzn/image/upload/v1684309186/SpacesBrandPage/Social_Icon_fx3pmg.png' alt='' />
+                        <img src='https://res.cloudinary.com/da3avivzn/image/upload/v1686301034/SpacesBrandPage/Social_Icon_mbtedy.png' alt='' />
                     </a>
                     <a href='https://instagram.com/spaces_ui?igshid=MmJiY2I4NDBkZg==' target="_blank" rel="noopener noreferrer" >
-                        <img src='https://res.cloudinary.com/da3avivzn/image/upload/v1684309189/SpacesBrandPage/Social_Icon_1_z1fo9d.png' alt='' />
+                        <img src='https://res.cloudinary.com/da3avivzn/image/upload/v1686300992/SpacesBrandPage/Social_Icon2-removebg-preview_quprah.png' alt='' />
                     </a>
-                    <a>
-                        <img src='https://res.cloudinary.com/da3avivzn/image/upload/v1684309191/SpacesBrandPage/Social_Icon_2_snnchi.png' alt='' />
-
+                    <a href='https://wa.link/lwcwp7'>
+                        <img src='https://res.cloudinary.com/da3avivzn/image/upload/v1686300993/SpacesBrandPage/Social_Icon3-removebg-preview_tqhau3.png' alt='' />
                     </a>
+                </div>
+                <div>
+                    <p>Email: Simplicityspaces@gmail.com</p>
                 </div>
                 <div>
                     <span> 2023 Spaces. All rights reserved</span>
--- src/Components/Projects/Projectlist.js
+++ src/Components/Projects/Projectlist.js
@@ -9,6 +9,7 @@ const ProjectList = [
         quote: '"The future is not a destination, its a direction." - Zig Ziglar',
         id: 'turn',
         color: '#071728',
+        url: 'https://cspaces1.netlify.app/'
     }, {
         title: 'Simply Cards',
         note: 'Join us as we delve into the intriguing and complex world of future predictions. Despite popular beliefs, accurately foretelling the future is not an exact science and is greatly influenced by numerous unpredictable variables and human free will',
@@ -17,25 +18,35 @@ const ProjectList = [
         type: 'Startup',
         id: 'cards',
         color: '#cc9a32',
-
         quote: '"Wealth is not about having a lot of money, its about having a lot of options." - Chris Rock',
         img: 'https://res.cloudinary.com/dt9f36crx/image/upload/v1675586132/spacesbrandpages/Image_2-5-23_at_5.28_AM_wjvoxk.jpg',
+        url: 'https://spaces-crediitcard.netlify.app/'
+    }, {
+        title: 'Sent By Anon',
+        note: 'We are thrilled to introduce our revolutionary anonymous payment feature that prioritizes your privacy and security. With our cutting-edge technology, you can now enjoy seamless and confidential transactions like never before. 💫🔒',
+        designPlatform: 'Live',
+        date: 'Present',
+        type: 'Finance',
+        quote: 'stayANON',
+        color: '#ac79b0',
+        id: 'Finance',
 
-    }, {
-        title: '',
-        note: '',
-        designPlatform: '',
-        date: '',
-        type: '',
-        quote: '',
-    }, {
-        title: '',
-        note: '',
-        designPlatform: '',
-        date: '',
-        type: '',
-        quote: '',
+        url: 'https://sentbyanon.com',
+        img: 'https://res.cloudinary.com/da3avivzn/image/upload/v1686308078/SpacesBrandPage/3cab9591-1a08-46ea-bb66-ff0253fb312d_ihkal9.jpg'    
     },
+    {
+        title: 'A Data Analyst Portfolio',
+        note: "I`m Ugwuidu Emmanuel.A Data Analyst who turns data  into insights that drive decisions.",
+        designPlatform: 'Live',
+        date: 'Data Analytics',
+        type: 'Work',
+        quote: 'Emmy9ce',
+        color: '#0197F6',
+        id: 'Portfolio',
+        url: 'https://9celytics.netlify.app/',
+        img: 'https://res.cloudinary.com/da3avivzn/image/upload/v1686312229/SpacesBrandPage/Screen_Shot_2023-06-09_at_5.02.18_AM_roxha1.png'    
+    },
+
 ]
 
 export default ProjectList
